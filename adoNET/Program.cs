30057a3b0eaf915290a0bbd7e3dc4598c38eb83d
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-10-24 21:53:01 +0500

    Updated

--- adoNET/Program.cs
+++ adoNET/Program.cs
@@ -1,10 +1,33 @@
-﻿namespace adoNET
+﻿using System.Data.SqlClient;
+
+namespace adoNET
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=StudyCenterDB;Trusted_Connection=True;";
 
+            using(SqlConnection connect = new SqlConnection())
+            {
+                connect.ConnectionString = connectionString;
+
+                connect.Open();
+
+                int findId = 1;
+
+                string query = $"select * from UserTable";
+
+                SqlCommand sqlCommand = new SqlCommand(query, connect);
+
+                using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader["userId"]} {reader["firstName"]} {reader["lastName"]}");
+                    }
+                }
+            }
         }
     }
 }
