ddf811dc0b5f4a7b3a31015b84e10b2d4b6997fa
Sanjarbek <125897994+Berdikulov-571@users.noreply.github.com>
2023-10-25 16:03:20 +0500

    Crud Functions Created

---
+++ adoNET/DataContextAdoNet.cs
@@ -0,0 +1,135 @@
+﻿using System.Data.SqlClient;
+
+namespace adoNET
+{
+    public class DataContextAdoNet
+    {
+        /// <summary>
+        /// Get By Id
+        /// </summary>
+        /// <param name="userId"></param>
+        public static void GetById(int userId)
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=StudyCenterDB;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                string query = $"select * from userTable where userId = {userId}";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                using(SqlDataReader reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        Console.WriteLine($"{reader["userId"]} {reader["firstName"]} {reader["lastName"]}");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get All
+        /// </summary>
+        public static void GetAll()
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=StudyCenterDB;Trusted_Connection=True;";
+
+            using(SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                string query = "select * from userTable";
+
+                SqlCommand command = new SqlCommand(@query, connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while(reader.Read())
+                    {
+                        Console.WriteLine($"{reader["userId"]} {reader["firstName"]} {reader["lastName"]}");
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Delete User
+        /// </summary>
+        /// <param name="userId"></param>
+        public static void DeleteById(int userId)
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=StudyCenterDB;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                string query = $"delete from userTable where userId = {userId}";
+
+                SqlCommand command = new SqlCommand(query , connection);
+
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create new User
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        public static void Create(int userId,string firstName,string lastName)
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=StudyCenterDB;Trusted_Connection=True;";
+            
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                string query = $"insert into userTable(userId,firstName,lastName) values({userId}, '{firstName}', '{lastName}')";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                using (SqlDataReader reader = command.ExecuteReader()) { }
+            }
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        public static void Update(int userId, string firstName, string lastName)
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=StudyCenterDB;Trusted_Connection=True;";
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = connectionString;
+
+                connection.Open();
+
+                string query = $"update userTable set firstName = '{firstName}',lastName = '{lastName}' where userId = {userId};";
+
+                SqlCommand command = new SqlCommand(query,connection);
+
+                using(SqlDataReader reader = command.ExecuteReader()) { }
+            }
+        }
+    }
+}
